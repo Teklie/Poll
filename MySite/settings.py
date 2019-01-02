8018e69053d97796e9f0fe509b03460915e43e33
Teklie <teklie@student.chalmers.se>
2019-01-03 00:38:10 +0100

    First view is added

--- MySite/settings.py
+++ MySite/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Poll.urls'
+ROOT_URLCONF = 'MySite.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Poll.wsgi.application'
+WSGI_APPLICATION = 'MySite.wsgi.application'
 
 
 # Database
